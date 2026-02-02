1c29d4538d0088503a762390bec28a85ab3856bf
Cedrick <cpe.cedrickbaltazar@gmail.com>
2026-02-02 10:12:22 +0800

    initial commit

---
+++ XedTracker/XedTracker/XedTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  XedTrackerApp.swift
+//  XedTracker
+//
+//  Created by Cedrick on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct XedTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
